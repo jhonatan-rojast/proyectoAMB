b8efdb075b0bb22a606f46f32132d5e0c89b9626
Jhonatan Rojas <jhonatan.rojas.terrones@ibm.com>
2018-10-24 13:59:36 +0200

    Subiendo fichero restantes para ProyectoAMB

---
+++ src/test/java/com/ibm/amb/ProyectoAMB/ProyectoAmbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.amb.ProyectoAMB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoAmbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
