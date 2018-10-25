c4cf19c106a6c42a3d8c98da857eb511c9ae8a21
Jhonatan Rojas <jhonatan.rojas.terrones@ibm.com>
2018-10-25 14:15:38 +0200

    test

--- src/main/java/com/ibm/amb/controller/HelloController.java
+++ src/main/java/com/ibm/amb/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
 		@RequestMapping("/hello")
 		public String index() {
-			return "Hola mundo con Jenkins JODER!!!!!";
+			return "Hola mundo con Jenkins!!!!!";
 		}
 }
